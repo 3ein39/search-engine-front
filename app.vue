2823df97bb72f97de88d34ec0273b835ddd523d1
Hussein Hany <hussein.hany.cs@gmail.com>
2024-05-17 14:33:06 +0300

    Refactor search input and button styles

--- app.vue
+++ app.vue
@@ -1,99 +1,17 @@
 <template>
-  <main
-    class="min-h-screen bg-gray-800 flex justify-center items-center flex-col"
-  >
+  <main class="min-h-screen bg-gray-800 flex justify-center items-center flex-col">
     <div class="flex flex-col bg-[#2f3640] w-2/4 rounded-t-3xl">
       <div class="searchBox">
-        <input
-          class="searchInput border-b border-gray-400"
-          type="text"
-          name=""
-          placeholder="Search something"
-          v-model="value"
-        />
+        <input class="searchInput border-b border-gray-400" type="text" name="" placeholder="Search something"
+          v-model="value" @keyup.enter="searchValueChange" />
         <button class="searchButton" @click="searchValueChange">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="29"
-            height="29"
-            viewBox="0 0 29 29"
-            fill="none"
-          >
-            <g clip-path="url(#clip0_2_17)">
-              <g filter="url(#filter0_d_2_17)">
-                <path
-                  d="M23.7953 23.9182L19.0585 19.1814M19.0585 19.1814C19.8188 18.4211 20.4219 17.5185 20.8333 16.5251C21.2448 15.5318 21.4566 14.4671 21.4566 13.3919C21.4566 12.3167 21.2448 11.252 20.8333 10.2587C20.4219 9.2653 19.8188 8.36271 19.0585 7.60242C18.2982 6.84214 17.3956 6.23905 16.4022 5.82759C15.4089 5.41612 14.3442 5.20435 13.269 5.20435C12.1938 5.20435 11.1291 5.41612 10.1358 5.82759C9.1424 6.23905 8.23981 6.84214 7.47953 7.60242C5.94407 9.13789 5.08145 11.2204 5.08145 13.3919C5.08145 15.5634 5.94407 17.6459 7.47953 19.1814C9.01499 20.7168 11.0975 21.5794 13.269 21.5794C15.4405 21.5794 17.523 20.7168 19.0585 19.1814Z"
-                  stroke="white"
-                  stroke-width="3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  shape-rendering="crispEdges"
-                ></path>
-              </g>
-            </g>
-            <defs>
-              <filter
-                id="filter0_d_2_17"
-                x="-0.418549"
-                y="3.70435"
-                width="29.7139"
-                height="29.7139"
-                filterUnits="userSpaceOnUse"
-                color-interpolation-filters="sRGB"
-              >
-                <feFlood
-                  flood-opacity="0"
-                  result="BackgroundImageFix"
-                ></feFlood>
-                <feColorMatrix
-                  in="SourceAlpha"
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                  result="hardAlpha"
-                ></feColorMatrix>
-                <feOffset dy="4"></feOffset>
-                <feGaussianBlur stdDeviation="2"></feGaussianBlur>
-                <feComposite in2="hardAlpha" operator="out"></feComposite>
-                <feColorMatrix
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                ></feColorMatrix>
-                <feBlend
-                  mode="normal"
-                  in2="BackgroundImageFix"
-                  result="effect1_dropShadow_2_17"
-                ></feBlend>
-                <feBlend
-                  mode="normal"
-                  in="SourceGraphic"
-                  in2="effect1_dropShadow_2_17"
-                  result="shape"
-                ></feBlend>
-              </filter>
-              <clipPath id="clip0_2_17">
-                <rect
-                  width="28.0702"
-                  height="28.0702"
-                  fill="white"
-                  transform="translate(0.403503 0.526367)"
-                ></rect>
-              </clipPath>
-            </defs>
-          </svg>
+          <FileSearchOutlined style="font-size: 24px;" />
         </button>
       </div>
       <div class="">
-        <transition-group
-          name="fade"
-          tag="div"
-          class="max-h-80 flex flex-col w-full overflow-y-auto"
-        >
-          <a
-            :href="item"
-            v-for="(item, index) in fakeContent"
-            :key="index"
-            class="text-white p-6 border-b-2 border-gray-700 text-left hover:bg-gray-700 flex items-center gap-4 truncate"
-          >
+        <transition-group name="fade" tag="div" class="max-h-80 flex flex-col w-full overflow-y-auto">
+          <a :href="item" v-for="(item, index) in fakeContent" :key="index"
+            class="text-white p-6 border-b-2 border-gray-700 text-left hover:bg-gray-700 flex items-center gap-4 truncate">
             {{ item }}
           </a>
         </transition-group>
@@ -120,10 +38,32 @@ const searchValueChange = async () => {
   transition: opacity 0.5s ease;
 }
 
+/* The emerging W3C standard that is currently Firefox only */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: rgba(155, 155, 155, 0.7) rgba(132, 124, 124, 0);
+}
+
+/* Works on Chrome/Edge/Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  background: rgba(12, 12, 12, 0.085);
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: rgba(155, 155, 155, 0.7);
+  border-radius: 20px;
+  border: 3px solid rgba(0, 0, 0, 0.146);
+}
+
 .fade-enter,
 .fade-leave-to
 
-/* .fade-leave-active in <2.1.8 */ {
+/* .fade-leave-active in <2.1.8 */
+  {
   opacity: 0;
 }
 
@@ -144,16 +84,15 @@ const searchValueChange = async () => {
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  background: var(
-    --gradient-2,
-    linear-gradient(90deg, #2af598 0%, #009efd 100%)
-  );
+  background: var(--gradient-2,
+      linear-gradient(90deg, #2af598 0%, #009efd 100%));
   border: 0;
   display: flex;
   align-items: center;
   justify-content: center;
   transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
 }
+
 /*hover effect*/
 button:hover {
   color: #fff;
@@ -161,6 +100,7 @@ button:hover {
   box-shadow: rgba(0, 0, 0, 0.5) 0 10px 20px;
   transform: translateY(-3px);
 }
+
 /*button pressing effect*/
 button:active {
   box-shadow: none;
